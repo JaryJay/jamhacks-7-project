61488c2f8561cc0a91ca437ba8d98e43e8ab4ab3
zhanc2 <76535472+zhanc2@users.noreply.github.com>
2023-06-11 08:24:23 -0400

    Finish finding color

--- get_color.py
+++ get_color.py
@@ -8,7 +8,7 @@ import webcolors
 
 def closest_colour(requested_colour):
     min_colours = {}
-    for name, key in webcolors.CSS3_HEX_TO_NAMES.items():
+    for key, name in webcolors.CSS3_HEX_TO_NAMES.items():
         r_c, g_c, b_c = webcolors.hex_to_rgb(key)
         rd = (r_c - requested_colour[0]) ** 2
         gd = (g_c - requested_colour[1]) ** 2
@@ -20,17 +20,17 @@ def closest_colour(requested_colour):
 def get_colour_name(requested_colour):
     try:
         closest_name = webcolors.rgb_to_name(requested_colour)
-    except ValueError:
+    except:
         closest_name = closest_colour(requested_colour)
     return closest_name
 
 
-def show_img_compar(img_1, img_2 ):
-    f, ax = plt.subplots(1, 2, figsize=(10,10))
+def show_img_compar(img_1, img_2):
+    f, ax = plt.subplots(1, 2, figsize=(10, 10))
     ax[0].imshow(img_1)
     ax[1].imshow(img_2)
-    ax[0].axis('off') #hide the axis
-    ax[1].axis('off')
+    ax[0].axis("off")  # hide the axis
+    ax[1].axis("off")
     f.tight_layout()
     plt.show()
 
@@ -38,16 +38,16 @@ def show_img_compar(img_1, img_2 ):
 def palette(clusters):
     palette = np.zeros((1, clusters.cluster_centers_.shape[0], 3), np.uint8)
     steps = 1
-    for idx, centers in enumerate(clusters.cluster_centers_): 
-        palette[:, int(idx*steps):(int((idx+1)*steps)), :] = centers
+    for idx, centers in enumerate(clusters.cluster_centers_):
+        palette[:, int(idx * steps) : (int((idx + 1) * steps)), :] = centers
     return palette
 
 
-def get_colors():
-    im = Image.open("ss3.jpg")
+def get_colors(img_path):
+    im = Image.open(img_path)
     w, h = im.size
 
-    im_cropped= im.crop((w//3, h//8, 2*w//3, h))
+    im_cropped = im.crop((w // 3, h // 8, 2 * w // 3, h))
     # im_cropped = im
     img = np.asarray(im_cropped)
 
@@ -59,23 +59,27 @@ def get_colors():
 
     dim = (500, 300)
     # resize image
-    img = cv.resize(img, dim, interpolation = cv.INTER_AREA)
+    img = cv.resize(img, dim, interpolation=cv.INTER_AREA)
     # img_2 = cv.resize(img_2, dim, interpolation = cv.INTER_AREA)
 
     clt = KMeans(n_clusters=3)
-    
+
     clt_1 = clt.fit(img.reshape(-1, 3))
-    img_palette = palette(clt_1)
-    # print(img_palette)
+    img_palette = palette(clt_1)[0]
 
-    darkest_sum, index = img_palette[0][0][0] + img_palette[0][0][1] + img_palette[0][0][2], 0
-    for i in range(1, len(img_palette)):
-        if darkest_sum < img_palette[0][i][0] + img_palette[0][i][1] + img_palette[0][i][2]:
-            darkest_sum, index = img_palette[0][i][0] + img_palette[i][1] + img_palette[0][i][2], i
-    
-    color = img_palette[index]
+    darkest_color = (
+        int(img_palette[0][0]),
+        int(img_palette[0][1]),
+        int(img_palette[0][2]),
+    )
 
-    color_name = get_colour_name((color[0], color[1], color[2]))
+    for i in img_palette:
+        if darkest_color[0] + darkest_color[1] + darkest_color[2] > int(i[0]) + int(
+            i[1]
+        ) + int(i[2]):
+            darkest_color = (int(i[0]), int(i[1]), int(i[2]))
+
+    color_name = get_colour_name(darkest_color)
 
     return color_name
 
@@ -85,4 +89,4 @@ def get_colors():
 
 
 if __name__ == "__main__":
-    print(get_colors())
+    print(get_colors("a.png"))
