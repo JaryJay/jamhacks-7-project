b13b92a174a83c805a285d6e7db923361974442a
Dissident <kak100kak@gmail.com>
2023-06-10 20:17:35 -0400

    Get bounding boxes and labels

--- clothing_detector.py
+++ clothing_detector.py
@@ -25,7 +25,6 @@ class TestConfig(Config):
 
 config = TestConfig()
 
-
 model = modellib.MaskRCNN(
     mode="inference", config=config, model_dir=os.getcwd() + "/logs"
 )
@@ -48,20 +47,62 @@ class_names = [
     "",
 ]
 
+score_threshold = 0.5  # Set the minimum score threshold
+max_results = 2  # Set the maximum number of results for each class
+
 cap = cv2.VideoCapture(0)
 while cap.isOpened():
     success, image = cap.read()
     if not success:
         print("L + ratio")
         continue
+    image = cv2.flip(image, 2)
     image.flags.writeable = False
 
     results = model.detect([image], verbose=0)
 
-    print(results)
+    r = results[0]
 
-    cv2.imshow("Outfit Oracle", cv2.flip(image, 2))
+    # Create a dictionary to store unique class labels and their corresponding bounding boxes
+    unique_labels = {}
+
+    for i in range(r["rois"].shape[0]):
+        class_id = r["class_ids"][i]
+        class_name = class_names[class_id]
+        score = r["scores"][i]
+        bbox = r["rois"][i]
+
+        if score >= score_threshold:  # Filter out results below the threshold
+            if class_name not in unique_labels:
+                unique_labels[class_name] = []
+            unique_labels[class_name].append((score, bbox))
+
+    # Draw the limited number of bounding boxes and labels on the image
+    for class_name, detections in unique_labels.items():
+        detections = sorted(detections, key=lambda x: x[0], reverse=True)[
+            :max_results
+        ]  # Sort and limit detections
+
+        color = (0, 255, 0)  # Green color for bounding boxes
+
+        for score, bbox in detections:
+            cv2.rectangle(image, (bbox[1], bbox[0]), (bbox[3], bbox[2]), color, 2)
+
+            label = "{}: {:.2f}".format(class_name, score)  # Move label creation here
+            y = bbox[0] + 20
+            cv2.putText(
+                image,
+                label,
+                (bbox[1], y),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.7,
+                color,
+                2,
+            )
+
+    cv2.imshow("Outfit Oracle", image)
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 cap.release()
+cv2.destroyAllWindows()
