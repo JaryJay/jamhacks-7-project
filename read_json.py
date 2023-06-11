dc08c6a65df7fd2dc600ead54a237ea96e8a9bb5
zhanc2 <76535472+zhanc2@users.noreply.github.com>
2023-06-10 23:43:29 -0400

    Create read_json.py

---
+++ read_json.py
@@ -0,0 +1,34 @@
+import json
+import requests
+from PIL import Image
+from numpy import asarray
+
+def get_data():
+    f = open('data.json')
+
+    data = json.load(f)
+
+    desiredTrainImgNum = 5000
+    c = 1000000 // desiredTrainImgNum
+
+    listOfImgs = []
+    listOfLabels = []
+
+    for img_num, img_data in enumerate(data['images']):
+        if img_num % c == 0:
+            img = Image.open(requests.get(img_data['url'], stream=True).raw)
+            numpydata = asarray(img)
+            listOfImgs.append(numpydata)
+    
+    for annotation_num, annotation_data in enumerate(data['annotations']):
+        if annotation_num % c == 0:
+            labelIds = annotation_data.labelId
+            listOfLabels.append(labelIds)
+    
+    result = []
+    for i in range(len(listOfImgs)):
+        result.append({
+            "img": listOfImgs[i],
+            "labels": listOfLabels[i]
+        })
+    return result
